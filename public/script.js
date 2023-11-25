330b531981994a5fbe57a0ef18b3752a88da9673
khalvai <khalvayozbek@gmail.com>
2023-11-25 21:06:43 +0330

    fix : make more attractive the buttons

--- public/script.js
+++ public/script.js
@@ -1,13 +1,13 @@
 // public/script.js
 document.addEventListener('DOMContentLoaded', function () {
-
-  const nextButton = document.getElementById('next');
-  
-  nextButton.addEventListener('click', function () {
+  const next = document.getElementById('next');
+  next.addEventListener('click', function () {
     fetch('/next');
   });
 
-  document.getElementById('previous').addEventListener('click', function () {
+  const previous = document.getElementById('previous');
+
+  previous.addEventListener('click', function () {
     fetch('/previous');
   });
 });
